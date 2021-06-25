3055b0a9ebda835d7018e16f12f748977cb370c2
Nikita <niklas.buk@gmail.com>
2021-06-25 15:53:49 +0700

    homework 6 #

---
+++ 2/script/data.js
@@ -0,0 +1,8 @@
+var Item = function NewItem (title, price) {
+   this.title = title;
+   this.price = price;
+}
+
+var item_1 = new Item ('title-1', 1000),
+   item_2 = new Item ('title-2', 1500),
+   item_3 = new Item ('title-3', 400),
